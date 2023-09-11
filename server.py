9b16c58416468ba942aec76ac59690b0eccf9548
kyfgcodes <kylegasserfernando@gmail.com>
2023-09-11 11:07:22 +0200

    removed comment

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ def my_home():
     return render_template("index.html")
 
 
-@app.route("/<string:page_name>") #uyihyiyy
+@app.route("/<string:page_name>") 
 def html_page(page_name):
     return render_template(page_name)
 
